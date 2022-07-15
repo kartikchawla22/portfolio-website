339d0094e7a331965319463a489b20a55b197545
Kartik Chawla <kchawla1995@gmail.com>
2022-07-15 01:19:19 -0400

    about me and project edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,32 +16,6 @@ import { routeTransitionAnimations } from './utils/animations';
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('drawer') drawer!: MatSidenav;
-  links = [
-    {
-      name: 'github',
-      image: 'assets/images/logo/github.png',
-      url: 'https://github.com/kartikchawla22/',
-      class: 'two'
-    },
-    {
-      name: 'linkedin',
-      image: 'assets/images/logo/linkedin.png',
-      url: 'https://www.linkedin.com/in/kartik-chawla-b51ba5118/',
-      class: 'three'
-    },
-    {
-      name: 'email',
-      image: 'assets/images/logo/email.png',
-      url: 'mailto:kchawla1995@gmail.com',
-      class: 'four'
-    },
-    {
-      name: 'resume',
-      image: 'assets/images/logo/experience.png',
-      url: 'assets/documents/resume.pdf',
-      class: 'five'
-    }
-  ]
   constructor(private _breakpointObserver$: BreakpointObserver) { }
   ngAfterViewInit(): void {
     this._breakpointObserver$.observe(['(max-width: 800px)']).subscribe((res) => {
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -12,9 +12,24 @@ export class ProjectComponent implements OnInit {
   projects: any = {
     rideShare: {
       imageNames: ['Ride Share-01', 'Ride Share-02', 'Ride Share-03', 'Ride Share-04', 'Ride Share-05', 'Ride Share-06', 'Ride Share-07', 'Ride Share-08', 'Ride Share-09', 'Ride Share-10', 'Ride Share-11', 'Ride Share-12', 'Ride Share-13'],
-      figmaLink: '',
       title: "Ride Share",
-      name: 'rideShare'
+      name: 'rideShare',
+      description: `Now a days everybody needs to go from place to place, but not everybody can have a personal vehical. Taking public transport such as buses can be hectic and taking a taxi can be very expensive.
+      We have came up with the solution for this. Ride Share allows the users to share their ride with other users.`,
+      details: [
+        {
+          key: "Role:", value: "Development Lead"
+        },
+        {
+          key: "Technology:", value: "React-Native"
+        },
+        {
+          key: "Fonts:", value: "Arial"
+        },
+        {
+          key: "Platforms:", value: "Androind, iOS"
+        }
+      ]
     }
   }
   constructor(private _route: ActivatedRoute) { }
