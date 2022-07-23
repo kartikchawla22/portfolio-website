e585a3c0cf58d561a9fd2317f70c94d55c97e63b
Kartik Chawla <kchawla1995@gmail.com>
2022-07-23 17:39:57 -0400

    removed api key

--- src/app/services/email/email.service.ts
+++ src/app/services/email/email.service.ts
@@ -10,7 +10,6 @@ export class EmailService {
 
   sendEmail(data: any) {
     let headers = new HttpHeaders();
-    headers = headers.set("Authorization", "Bearer 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");
     headers = headers.set("Content-Type", "application/json");
     return this._http.post("https://api.sendgrid.com/v3/mail/send", data, { headers });
   }
